a4e7270f920ce68d6527787ccd7b909a9abf1207
helal majid <helalmajid@helals-iMac.attlocal.net>
2015-06-28 15:24:09 -0700

    This is my first commit

---
+++ HelloWorld/person.swift
@@ -0,0 +1,21 @@
+//
+//  person.swift
+//  HelloWorld
+//
+//  Created by helal majid on 6/28/15.
+//  Copyright (c) 2015 billywong. All rights reserved.
+//
+
+import Foundation
+
+class Person
+{
+    
+    var firstName:String;
+    
+    init(firstName:String){
+        
+        self.firstName=firstName;
+    
+   }
+}
